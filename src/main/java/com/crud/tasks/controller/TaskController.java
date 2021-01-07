b94ae5ba131498df59f8aa5e993c3d221d07bc3d
LZdziechowski <l.zdziechowski.contact@gmail.com>
2021-01-07 18:41:21 +0100

    15.3 Adding methods to TaskController class

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -5,10 +5,9 @@ import com.crud.tasks.domain.TaskDto;
 import com.crud.tasks.mapper.TaskMapper;
 import com.crud.tasks.service.DbService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.expression.spel.ast.OpAnd;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -32,26 +31,31 @@ public class TaskController {
     }
 
     @GetMapping(value = "getTask")
-    public TaskDto getTask(@RequestParam Long taskId) {
-        Optional<Task> optionalTask = service.getTask(taskId);
-        if(optionalTask.isPresent()) {
-            Task task = optionalTask.get();
-            return taskMapper.mapToTaskDto(task);
-        } else {
-            return null;
-        }
+    public TaskDto getTask(@RequestParam Long taskId) throws TaskNotFoundException {
+        return taskMapper.mapToTaskDto(
+                service.getTask(taskId).orElseThrow(TaskNotFoundException::new)
+        );
     }
 
-    @DeleteMapping(value = "deleteTask")
-    public void deleteTask(Long taskId) {
+    @PostMapping(value = "createTask", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void createTask(@RequestBody TaskDto taskDto) {
+        Task task = taskMapper.mapToTask(taskDto);
+        service.saveTask(task);
     }
 
     @PutMapping(value = "updateTask")
-    public TaskDto updateTask(TaskDto taskDto) {
-        return new TaskDto(1L, "Edited test title", "Test content");
+    public TaskDto updateTask(@RequestBody TaskDto taskDto) {
+        Task task = taskMapper.mapToTask(taskDto);
+        Task savedTask = service.saveTask(task);
+        return taskMapper.mapToTaskDto(savedTask);
     }
 
-    @PostMapping(value = "createTask")
-    public void createTask(TaskDto taskDto) {
+    @DeleteMapping(value = "deleteTask")
+    public void deleteTask(@RequestParam Long taskId) throws TaskNotFoundException{
+        try {
+            service.deleteTask(taskId);
+        } catch (NullPointerException e) {
+            throw new TaskNotFoundException();
+        }
     }
 }
