a9f2ab75f7a84f404a46a256220e57c4d0bc1d79
LZdziechowski <l.zdziechowski.contact@gmail.com>
2021-03-08 17:01:55 +0100

    Add application functionality list to mail informing about created Trello card

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import java.util.*;
+
 @Service
 public class MailCreatorService {
 
@@ -31,17 +33,24 @@ public class MailCreatorService {
     private TemplateEngine templateEngine;
 
     public String buildTrelloCardEmail(String message) {
+
+        List<String> functionality = new ArrayList<>();
+        functionality.add("You can manage your tasks");
+        functionality.add("Provides connection with Trello Account");
+        functionality.add("Application allows sending tasks to Trello");
+
         Context context = new Context();
         context.setVariable("message", message);
         context.setVariable("tasks_url", "https://lzdziechowski.github.io/");
         context.setVariable("button", "Visit website");
-        context.setVariable("admin_name", adminConfig.getAdminName());
+        context.setVariable("admin_name", adminConfig);
         context.setVariable("company_name", companyName);
         context.setVariable("company_phone", companyPhone);
         context.setVariable("company_email", companyEmail);
         context.setVariable("company_goal", companyGoal);
-        context.setVariable("show_button", false);
+        context.setVariable("show_button", true);
         context.setVariable("is_friend", true);
+        context.setVariable("application_functionality", functionality);
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
 }
