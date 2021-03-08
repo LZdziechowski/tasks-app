065a1f29e63bae92509c53a6c5ad07ff0d8831da
LZdziechowski <l.zdziechowski.contact@gmail.com>
2021-03-08 15:13:48 +0100

    email schema done

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -3,12 +3,25 @@ package com.crud.tasks.service;
 import com.crud.tasks.config.AdminConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
 @Service
 public class MailCreatorService {
+
+    @Value("${info.company.name}")
+    private String companyName;
+
+    @Value("${info.company.email}")
+    private String companyEmail;
+
+    @Value("${info.company.phone}")
+    private String companyPhone;
+
+    @Value("${info.company.goal}")
+    private String companyGoal;
 
     @Autowired
     private AdminConfig adminConfig;
@@ -23,6 +36,8 @@ public class MailCreatorService {
         context.setVariable("tasks_url", "https://lzdziechowski.github.io/");
         context.setVariable("button", "Visit website");
         context.setVariable("admin_name", adminConfig.getAdminName());
+        context.setVariable("signature", ("\n" + companyName));
+        context.setVariable("company_details", (companyName + "\n" + companyPhone + "\n" + companyEmail + "\n" + companyGoal));
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
 }
